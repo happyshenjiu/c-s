faaaa2b3853d6dd42ff23e7aa10cc7b927a6427f
余金花 <yujinhua@ifoxtec.com>
2017-03-14 17:15:57 +0800

    补充样式

--- wp-content/themes/clean-corporate/inc/hook/slider.php
+++ wp-content/themes/clean-corporate/inc/hook/slider.php
@@ -156,6 +156,7 @@ if ( ! function_exists( 'clean_corporate_render_featured_slider' ) ) :
 		);
 		if ( $featured_slider_enable_caption ) {
 //			$slide_data['caption-template'] = '<h3><a href="{{url}}" target="{{target}}">{{title}}</a></h3><p>{{excerpt}}</p>{{buttons}}';
+			
 //			注释首页广告部分显示页面简介
 			$slide_data['caption-template'] = '<h3><a href="{{url}}" target="{{target}}">{{title}}</a></h3>{{buttons}}';
 		}
