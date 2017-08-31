bceb22b3200d02554b0e23e0f1636c8d87922145
余金花 <yujinhua@ifoxtec.com>
2017-08-31 11:24:52 +0800

    contact form 7

--- wp-content/plugins/contact-form-7/modules/textarea.php
+++ wp-content/plugins/contact-form-7/modules/textarea.php
@@ -73,7 +73,7 @@ function wpcf7_textarea_form_tag_handler( $tag ) {
 	$atts = wpcf7_format_atts( $atts );
 
 	$html = sprintf(
-		'<span class="wpcf7-form-control-wrap %1$s"><textarea %2$s onfocus="this.placeholder= \'\' " onblur="this.placeholder= \''.empty( $tag->content )? (string) reset( $tag->values ): $tag->content.'\'">%3$s</textarea>%4$s</span>',
+		'<span class="wpcf7-form-control-wrap %1$s"><textarea %2$s onfocus="this.placeholder= \'\' " onblur="this.placeholder= \''.(string) reset( $tag->values ).'\'">%3$s</textarea>%4$s</span>',
 		sanitize_html_class( $tag->name ), $atts,
 		esc_textarea( $value ), $validation_error );
 
